87130734166b883894b498bb9869a7402133c82b
ria05 <riyaakanojia05@gmail.com>
2025-07-16 19:03:28 +0530

    created login page in working state, home page also created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   }
 ];
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  clientId: "ComplaintApp",
+  clientSecretStaging: "H3RcUHUCQkzPrilWCVIQpi4BUr1E7AUG",
+  keycloakUrlStaging: "http://localhost:8080/realms/ionic-project/protocol/openid-connect/token",
 };
 
 /*
